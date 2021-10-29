40c22ed8867d94d4da6fff7654f18ed1a14a3b75
199ocero <57591686+199ocero@users.noreply.github.com>
2021-10-29 15:14:50 +0800

    bot updates

--- routes/botman.php
+++ routes/botman.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\BotManController;
 $botman = resolve('botman');
 
 $botman->hears('GET_STARTED', function ($bot) {
-    $bot->typesAndWaits(1);
     $firstName = $bot->getUser()->getFirstName();
     $senderId = $bot->getUser()->getId();
     $input = json_decode(file_get_contents('php://input'), true);
@@ -15,13 +14,15 @@ $botman->hears('GET_STARTED', function ($bot) {
     $bot->reply('👋Hello! This is UPortal, a hussle free access to university portal.');
 });
 $botman->hears('FB_PAYLOAD', function ($bot) {
-    $bot->typesAndWaits(1);
     $firstName = $bot->getUser()->getFirstName();
     $senderId = $bot->getUser()->getId();
+    if($senderId==3635465636550195){
+        $bot->reply('👋Hello Jay-Are Main Account');
+    }elseif($senderId==3635465636550195){
+        $bot->reply('👋Hello Jay-Are Second Account');
+    }else{
+        $bot->reply('👋Hello Not Registered');
+    }
     
-    // $bot->reply("Hi $firstName 👋!\n\nI think this is your first time using this chatbot right?\n\nI am 🤖UPortal🤖 giving you free access to University Portal.\n\nNow, I will give your Facebook ID. Copy your ID and paste it in your student's portal.");
-    $bot->reply('Facebook ID: '.$senderId);
-
-    $bot->reply('👋Hello! This is UPortal, a hussle free access to university portal.');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
