69595efc354703199c635f545a7a17b5894aea3a
199ocero <57591686+199ocero@users.noreply.github.com>
2021-10-29 14:50:43 +0800

    botman replies

--- routes/botman.php
+++ routes/botman.php
@@ -4,6 +4,12 @@ use App\Http\Controllers\BotManController;
 $botman = resolve('botman');
 
 $botman->hears('GET_STARTED', function ($bot) {
-    $bot->reply('👋Hello! This is UPortal, a hussle free access to university portal.');
+    $bot->typesAndWaits(1);
+    $firstName = $bot->getUser()->getFirstName();
+    $senderId = $bot->getUser()->getId();
+    $bot->reply("Hi $firstName 👋!\n\nI think this is your first time using this chatbot right?\n\nI am 🤖UPortal🤖 giving you free access to University website.\n\nNow, I will give your Facebook ID and you just have to present it to the registrar.");
+    $bot->reply('Facebook ID: '.$senderId);
+
+    // $bot->reply('👋Hello! This is UPortal, a hussle free access to university portal.');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
