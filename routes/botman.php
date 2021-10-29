d28f37cbf34069409fc349d8ff8e85c438b6fa97
199ocero <57591686+199ocero@users.noreply.github.com>
2021-10-29 14:00:53 +0800

    final release

--- routes/botman.php
+++ routes/botman.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('Jay-Are', function ($bot) {
-    $bot->reply('Gwapo!');
+$botman->hears('GET_STARTED', function ($bot) {
+    $bot->reply('👋Hello! This is UPortal, a hussle free access to university portal.');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
