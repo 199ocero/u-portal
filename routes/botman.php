5451fb8fb18b6c76da0aa0a23de8cd5f1e8ca1c5
199ocero <57591686+199ocero@users.noreply.github.com>
2021-11-01 17:38:59 +0800

    fallback

--- routes/botman.php
+++ routes/botman.php
@@ -112,7 +112,7 @@ $botman->hears('ANNOUNCEMENT_PAYLOAD', function ($bot) {
 
 $botman->fallback(function ($bot) {
     $extras = $bot->getMessage()->getExtras();
-    $intents = $extras['intents'];
+    $intents = $extras['intents']??null;
     $bot->reply("Sorry, I can\'t understand this command. Please type 'Commands' and hit send. $intents");
 });
 // $botman->fallback(function (BotMan $bot) {
