e19b6f87c71bf9bff99d4359937ccaa2612d4306
199ocero <57591686+199ocero@users.noreply.github.com>
2021-11-04 08:09:39 +0800

    add announcement

--- resources/views/pages/master/master.blade.php
+++ resources/views/pages/master/master.blade.php
@@ -15,9 +15,8 @@
 	<link rel="stylesheet" href="{{asset('backend/css/vendors_css.css')}}">
 	  
 	<!-- Style-->  
-	{{-- <link rel="stylesheet" href="{{asset('backend/css/style.css')}}"> --}}
-	<link rel="stylesheet" href="https://res.cloudinary.com/dv4dyq4ca/raw/upload/v1635982104/style_pybnl1.css">
-	<link rel="stylesheet" href="https://res.cloudinary.com/dv4dyq4ca/raw/upload/v1635983960/style_nipgtx.css">
+	<link rel="stylesheet" href="{{asset('backend/css/style.css')}}">
+	<link rel="stylesheet" href="{{asset('backend/css/skin_color.css')}}">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
   </head>
 
@@ -45,18 +44,19 @@
   	
 	
 	<!-- Vendor JS -->
-	<script src="https://res.cloudinary.com/dv4dyq4ca/raw/upload/v1635984016/vendors.min_ojuln1.js"></script>
-  <script src="https://res.cloudinary.com/dv4dyq4ca/raw/upload/v1635984069/feather.min_xc9e92.js"></script>	
-	<script src="https://res.cloudinary.com/dv4dyq4ca/raw/upload/v1635984117/jquery.easypiechart_hjd3ur.js"></script>
-	<script src="https://res.cloudinary.com/dv4dyq4ca/raw/upload/v1635984192/irregular-data-series_bsil3o.js"></script>
-	<script src="https://res.cloudinary.com/dv4dyq4ca/raw/upload/v1635984238/apexcharts_uvxq7u.js"></script>
-	<script src="https://res.cloudinary.com/dv4dyq4ca/raw/upload/v1635984290/datatables.min_xvszw9.js"></script>
-	<script src="https://res.cloudinary.com/dv4dyq4ca/raw/upload/v1635984317/data-table_sutbvh.js"></script>
-	<script src="https://res.cloudinary.com/dv4dyq4ca/raw/upload/v1635984348/jquery.toaster_lf6mgn.js"></script>
+	<script src="{{asset('backend/js/vendors.min.js')}}"></script>
+  <script src="{{asset('../assets/icons/feather-icons/feather.min.js')}}"></script>	
+	<script src="{{asset('../assets/vendor_components/easypiechart/dist/jquery.easypiechart.js')}}"></script>
+	<script src="{{asset('../assets/vendor_components/apexcharts-bundle/irregular-data-series.js')}}"></script>
+	<script src="{{asset('../assets/vendor_components/apexcharts-bundle/dist/apexcharts.js')}}"></script>
+	<script src="{{asset('../assets/vendor_components/datatable/datatables.min.js')}}"></script>
+	<script src="{{asset('backend/js/pages/data-table.js')}}"></script>
+	<script src="{{asset('backend/js/pages/jquery.toaster.js')}}"></script>
+  <script src="{{asset('../assets/vendor_components/dropzone/dropzone.js')}}"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
 	<!-- Sunny Admin App -->
-	<script src="https://res.cloudinary.com/dv4dyq4ca/raw/upload/v1635984375/template_s3xdpf.js"></script>
-	<script src="https://res.cloudinary.com/dv4dyq4ca/raw/upload/v1635984392/dashboard_f614qy.js"></script>
+	<script src="{{asset('backend/js/template.js')}}"></script>
+	<script src="{{asset('backend/js/pages/dashboard.js')}}"></script>
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 	
 	<script>
