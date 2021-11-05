30e350bfbce4f894e2f26c0a889885ea6f1471a4
199ocero <57591686+199ocero@users.noreply.github.com>
2021-11-05 20:27:35 +0800

    change profile design

--- resources/views/pages/instructor/profile/view-profile.blade.php
+++ resources/views/pages/instructor/profile/view-profile.blade.php
@@ -6,10 +6,11 @@
       <section class="content">
         <!-- /.row -->
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-12">
                 <div class="box box-widget widget-user">
                     <!-- Add the bg color to the header using any of the bg-* classes -->
                     <div class="widget-user-header bg-black">
+                      <h5>User Type: Instructor</h5>
                       <a href="{{route('view.profile.edit')}}" class="btn btn-primary float-right"><i class="glyphicon glyphicon-edit"></i> Edit Profile</a>
                     </div>
                     <div class="widget-user-image">
@@ -40,38 +41,37 @@
                           </div>
                           <!-- /.description-block -->
                         </div>
+                        
                         <!-- /.col -->
                       </div>
                       <!-- /.row -->
                     </div>
                     
                 </div>
-            </div>
-            <div class="col-md-6">
                 <div class="box">
-                    <div class="box-body box-profile">            
-                      <div class="row">
-                        <div class="col-12">
-                            <div>
-                                <p>Email :<span class="text-gray pl-10">{{$user->email}}</span></p>
-                                <p>Phone :<span class="text-gray pl-10">+11 123 456 7890</span></p>
-                                <p>Address :<span class="text-gray pl-10">123, Lorem Ipsum, Florida, USA</span></p>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="pb-15">						
-                                <p class="mb-10">Social Profile</p>
-                                <div class="user-social-acount">
-                                    <button class="btn btn-circle btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></button>
-                                    <button class="btn btn-circle btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></button>
-                                    <button class="btn btn-circle btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></button>
-                                </div>
-                            </div>
-                        </div>
+                  <div class="box-body box-profile">            
+                    <div class="row">
+                      <div class="col-12">
+                          <div>
+                              <p>Email :<span class="text-gray pl-10">{{$user->email}}</span></p>
+                              <p>Phone :<span class="text-gray pl-10">+11 123 456 7890</span></p>
+                              <p>Address :<span class="text-gray pl-10">123, Lorem Ipsum, Florida, USA</span></p>
+                          </div>
+                      </div>
+                      <div class="col-12">
+                          <div class="pb-15">						
+                              <p class="mb-10">Social Profile:</p>
+                              <div class="user-social-acount">
+                                  <button class="btn btn-circle btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></button>
+                                  <button class="btn btn-circle btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></button>
+                                  <button class="btn btn-circle btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></button>
+                              </div>
+                          </div>
                       </div>
                     </div>
-                    <!-- /.box-body -->
-                </div>
+                  </div>
+                  <!-- /.box-body -->
+              </div>
             </div>
             
             
