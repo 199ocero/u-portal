e92b1b9ae11239fcfe5dfe194cd4a5f726823882
Jay-Are Ocero <199ocero@gmail.com>
2022-06-04 11:19:01 +0800

    fix complete

--- routes/botman.php
+++ routes/botman.php
@@ -34,63 +34,60 @@ $botman->hears('PERSONAL_PAYLOAD', function ($bot) {
 });
 
 $botman->hears('ANNOUNCEMENT_PAYLOAD', function ($bot) {
-    $senderId = 3635465636550195;
-        $facebookID = Facebook::where('facebook_id',$senderId)->first();
-        $drop = Drop::where('student_id',$facebookID->student_id)->get();
-        $student = StudentSection::where('student_id',$facebookID->student_id)->get()->toArray();
-        $completes = Complete::where('student_id',$facebookID->student_id)->get();
-        $announcement= collect();
-        
-        if(count($drop)!=0){
+    $senderId = $bot->getUser()->getId();
+    $facebookID = Facebook::where('facebook_id',$senderId)->first();
+    $drop = Drop::where('student_id',$facebookID->student_id)->get();
+    $student = StudentSection::where('student_id',$facebookID->student_id)->get()->toArray();
+    $completes = Complete::where('student_id',$facebookID->student_id)->get();
+    $announcement= collect();
+    
+    if(count($drop)!=0){
 
-            $section_id = [];
-            $subject_id = [];
-            $completes_id=[];
+        $section_id = [];
+        $subject_id = [];
+        $completes_id=[];
 
-            foreach($drop as $drop){
-                $section_id[]=$drop->section_id;
-                $subject_id[]=$drop->subject_id;
-            }
-            foreach($completes as $completes){
-                $completes_id[]=$completes->announcement_id;
-            }
-            $announcement = Announcement::whereNotIn('section_id',$section_id)->whereNotIn('subject_id',$subject_id)->whereNotIn('id',$completes_id)->get();
-            dd($announcement);
-        }else{
-            for($i=0;$i<count($student);$i++){
-                $announce = Announcement::where('section_id',$student[$i]['section_id'])->get();
-
-                for($x=0;$x<count($announce);$x++){
-                    $announcement->push($announce[$x]);
-                }
-            }
-            $completes_id=[];
-            $announce_id=[];
-            foreach($completes as $completes){
-                $completes_id[]=$completes->announcement_id;
-            }
-            foreach($announcement as $announcement){
-                $announce_id[]=$announcement->id;
-            }
-            $final = array_diff($announce_id, $completes_id);
-            $announcement = Announcement::findMany($final);
-            dd($announcement->toArray());
+        foreach($drop as $drop){
+            $section_id[]=$drop->section_id;
+            $subject_id[]=$drop->subject_id;
         }
-        $anns = "";
-        if(count($announcement->toArray())==0){
-            $bot->reply("No announcement. You are either drop in all section/subject or did not register your Facebook ID.");
-        }else{
-            foreach($announcement as $announcement){
-                
-                $date = $announcement->deadline->format('F j, Y');
-                $time = $announcement->deadline->format('h:i A');
-                $section = $announcement['section']['section'];
-                $subject = $announcement['subject']['subject'];
-                $anns = "✅Section: $section\n✅Subject: $subject\n✅Date: $date\n✅Time: $time\n✅Activity Title: $announcement->act_title\n\n".$anns;
+        foreach($completes as $completes){
+            $completes_id[]=$completes->announcement_id;
+        }
+        $announcement = Announcement::whereNotIn('section_id',$section_id)->whereNotIn('subject_id',$subject_id)->whereNotIn('id',$completes_id)->get();
+    }else{
+        for($i=0;$i<count($student);$i++){
+            $announce = Announcement::where('section_id',$student[$i]['section_id'])->get();
+
+            for($x=0;$x<count($announce);$x++){
+                $announcement->push($announce[$x]);
             }
+        }
+        $completes_id=[];
+        $announce_id=[];
+        foreach($completes as $completes){
+            $completes_id[]=$completes->announcement_id;
+        }
+        foreach($announcement as $announcement){
+            $announce_id[]=$announcement->id;
+        }
+        $final = array_diff($announce_id, $completes_id);
+        $announcement = Announcement::findMany($final);
+    }
+    $anns = "";
+    if(count($announcement->toArray())==0){
+        $bot->reply("No announcement. You are either drop in all section/subject or did not register your Facebook ID.");
+    }else{
+        foreach($announcement as $announcement){
             
-            $bot->reply("📝Announcement Information📝\n\n$anns\n\nNote: If you want to see the full details of announcement please click the Visit Uportal button and login your credentials.");
+            $date = $announcement->deadline->format('F j, Y');
+            $time = $announcement->deadline->format('h:i A');
+            $section = $announcement['section']['section'];
+            $subject = $announcement['subject']['subject'];
+            $anns = "✅Section: $section\n✅Subject: $subject\n✅Date: $date\n✅Time: $time\n✅Activity Title: $announcement->act_title\n\n".$anns;
         }
+        $bot->reply("📝Announcement Information📝\n\n$anns\n\nNote: If you want to see the full details of announcement please click the Visit Uportal button and login your credentials.");
+    }
     
 });
 
